3df04b6b1ed67f721f2a350e0d71b459ef26c31a
zhyea <zhangrui@zhyea.com>
2026-07-22 16:26:51 +0800

    删除无用的内容

--- ts-port/node_modules/@vue/test-utils/dist/src/utils/componentName.d.ts
+++
@@ -1,3 +0,0 @@
-import type { ComponentInternalInstance, VNodeTypes } from 'vue';
-export declare const getComponentRegisteredName: (instance: ComponentInternalInstance | null, type: VNodeTypes) => string | null;
-export declare const getComponentName: (instance: any | null, type: VNodeTypes) => string;
